2c139f062eeb49b27a523f2897605aef9142bc6e
skkrtskkrt <76537413+skkrtskkrt@users.noreply.github.com>
2021-02-08 13:16:29 +0300

    Add files via upload

---
+++ gcrx.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const { Client, RichEmbed } = require("discord.js");
+const app = express();
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if (new Date().getTime() - start > milliseconds) {
+      break;
+    }
+  }
+}
+const http = require("http");
+app.get("/", (request, response) => {
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get("http://boost-aktif.glitch.me/");
+}, 3000);
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const data = new Map();88
+
+
+
+
+const swisha1 = new Discord.Client();
+const swisha2 = new Discord.Client();
+const swisha3 = new Discord.Client();
+const swisha4 = new Discord.Client();
+const swisha5 = new Discord.Client();
+const swisha6 = new Discord.Client();
+const swisha7 = new Discord.Client();
+const swisha8 = new Discord.Client();
+const swisha9 = new Discord.Client();
+const swisha10 = new Discord.Client();
+const swisha11 = new Discord.Client();
+const swisha12 = new Discord.Client();
+const swisha13 = new Discord.Client();
+const swisha14 = new Discord.Client();
+const swisha15 = new Discord.Client();
+const swisha16 = new Discord.Client();
+const swisha17= new Discord.Client();
+const swisha18 = new Discord.Client();
+const swisha19 = new Discord.Client();
+const swisha20 = new Discord.Client();
+const swisha21 = new Discord.Client();
+const swisha22 = new Discord.Client();
+const swisha23 = new Discord.Client();
+
+
+swisha1.login("");
+swisha2.login("");
+swisha3.login("");
+swisha4.login("");
+swisha5.login("");
+swisha6.login("");
+swisha7.login("");
+swisha8.login("");
+swisha9.login("");
+swisha10.login("");
+swisha11.login("");
+swisha12.login("");
+swisha13.login("");
+swisha14.login("");
+swisha15.login("");
+swisha16.login("");
+swisha17.login("");
+swisha18.login("");
+swisha19.login("");
+swisha20.login("");
